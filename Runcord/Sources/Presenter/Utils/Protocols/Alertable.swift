2afaeaf855fcca2c94af57ac58391335c64770ae
Kim-Junhwan <tazanboy1@naver.com>
2023-03-30 17:23:18 +0900

    [feat] Alert 기능 구현

--- Runcord/Sources/Presenter/Utils/Protocols/Alertable.swift
+++ Runcord/Sources/Presenter/Utils/Protocols/Alertable.swift
@@ -5,4 +5,17 @@
 //  Created by JunHwan Kim on 2023/03/30.
 //
 
-import Foundation
+import UIKit
+
+protocol Alertable {}
+
+extension Alertable where Self: UIViewController {
+    func showAlert(title: String = "", message: String, defaultActionTitle: String, cancelActionTitle: String, defaultActionBlock: ((UIAlertAction)->Void)? = nil) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let defaultAction = UIAlertAction(title: defaultActionTitle, style: .default, handler: defaultActionBlock)
+        let cancelAction = UIAlertAction(title: cancelActionTitle, style: .cancel)
+        alert.addAction(cancelAction)
+        alert.addAction(defaultAction)
+        self.present(alert, animated: true)
+    }
+}
