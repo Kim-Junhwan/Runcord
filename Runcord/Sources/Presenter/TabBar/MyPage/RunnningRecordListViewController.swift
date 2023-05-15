0a50a06f2048cd9ee406c105377036d5085cbece
Kim-Junhwan <tazanboy1@naver.com>
2023-05-15 17:50:58 +0900

    [refactor] MypageViewController 에서 RunningRecordListViewController로 이름 변경

---
+++ Runcord/Sources/Presenter/TabBar/MyPage/RunnningRecordListViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MyPageViewController.swift
+//  RunningCrew
+//
+//  Created by JunHwan Kim on 2023/02/13.
+//
+
+import UIKit
+
+class RunnningRecordListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        self.title = "러닝기록"
+    }
+
+}
