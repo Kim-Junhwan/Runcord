85f3af62a9625cda3a9cc567473f9cb7c601a79b
Kim-Junhwan <tazanboy1@naver.com>
2023-05-22 23:31:17 +0900

    [feat] 러닝 기록 리스트 러닝 정보 바인딩

--- Runcord/Sources/Presenter/TabBar/RunningRecordList/RunningRecordTableViewCell/RunningRecordTableViewCell.swift
+++ Runcord/Sources/Presenter/TabBar/RunningRecordList/RunningRecordTableViewCell/RunningRecordTableViewCell.swift
@@ -12,6 +12,13 @@ class RunningRecordTableViewCell: UITableViewCell {
     
     static let identifier = "RunningRecordTableViewCell"
     
+    @IBOutlet weak var runningYearLabel: UILabel!
+    @IBOutlet weak var runningStartMonthLabel: UILabel!
+    @IBOutlet weak var runningStartDayLabel: UILabel!
+    
+    @IBOutlet weak var runningStartHourLabel: UILabel!
+    @IBOutlet weak var runningStartMinuteLabel: UILabel!
+    
     @IBOutlet weak var runningRouteImageView: CustomRouteMapImageView!
     @IBOutlet weak var startDatelabel: UILabel!
     @IBOutlet weak var runningDistanceLabel: UILabel!
@@ -37,6 +44,25 @@ class RunningRecordTableViewCell: UITableViewCell {
     func setData(runningRecord: RunningRecord) {
         runningRouteImageView.setRouteImage(route: runningRecord.runningPath.map { CLLocationCoordinate2D(latitude: $0.latitude, longitude: $0.longitude) })
         runningDistanceLabel.text = String(format: "%.2f", runningRecord.runningDistance)
+        setDateLabel(runningRecord: runningRecord)
+        setRunningTimeLabel(runningRecord: runningRecord)
     }
     
+    private func setDateLabel(runningRecord: RunningRecord) {
+        let runningDate = runningRecord.date
+        let calender = Calendar.current
+        let components = calender.dateComponents([.year, .month, .day, .hour, .minute], from: runningDate)
+        runningYearLabel.text = String(describing: components.year!)
+        runningStartMonthLabel.text = String(describing: components.month!)
+        runningStartDayLabel.text = String(describing: components.day!)
+        
+        runningStartHourLabel.text = String(describing: components.hour!)
+        runningStartMinuteLabel.text = String(describing: components.minute!)
+    }
+    
+    private func setRunningTimeLabel(runningRecord: RunningRecord) {
+        let runningTime = runningRecord.runningTime
+        runningTimeHourLabel.text = String(format: "%02d", runningTime / 3600)
+        runningTimeMinuteLabel.text = String(format: "%02d", (runningTime % 3600) / 60)
+    }
 }
