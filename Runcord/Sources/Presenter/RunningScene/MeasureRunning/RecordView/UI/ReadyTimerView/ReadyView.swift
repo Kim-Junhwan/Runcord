2ebcd5134469bf9e170f23a64f11e564fd22f2fe
Kim-Junhwan <tazanboy1@naver.com>
2023-06-02 02:15:39 +0900

    [refactor] RecordView에서 러닝 준비 뷰 분리

---
+++ Runcord/Sources/Presenter/RunningScene/MeasureRunning/RecordView/UI/ReadyTimerView/ReadyView.swift
@@ -0,0 +1,72 @@
+//
+//  ReadyTimerView.swift
+//  Runcord
+//
+//  Created by JunHwan Kim on 2023/06/01.
+//
+
+import UIKit
+import Foundation
+
+class ReadyView: UIView {
+    
+    let sentenceLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+         label.textColor = .white
+         label.font = UIFont(name: "NotoSansKR-Medium", size: 30)
+        label.text = "러닝을 시작합니다"
+         
+         return label
+     }()
+
+    lazy var timerLabel: UILabel = {
+       let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .white
+        label.font = UIFont(name: "NotoSansKR-Bold", size: 130)
+        label.text = "\(readyTime)"
+        return label
+    }()
+    
+    private var timer: Timer?
+    
+    private var readyTime: Int {
+        didSet {
+            self.timerLabel.text = String(readyTime)
+        }
+    }
+    
+    init(readyTime: Int) {
+        self.readyTime = readyTime
+        super.init(frame: .zero)
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setLayout() {
+        addSubview(sentenceLabel)
+        addSubview(timerLabel)
+        
+        NSLayoutConstraint.activate([
+            timerLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            timerLabel.centerYAnchor.constraint(equalTo: centerYAnchor),
+            
+            sentenceLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            sentenceLabel.bottomAnchor.constraint(equalTo: timerLabel.topAnchor, constant: -30.0)
+        ])
+    }
+    
+    func startPrepare(completion: @escaping () -> Void) {
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true, block: { [weak self] timer in
+            self?.readyTime -= 1
+            if self?.readyTime == 0 {
+                timer.invalidate()
+                completion()
+            }
+        })
+    }
+}
