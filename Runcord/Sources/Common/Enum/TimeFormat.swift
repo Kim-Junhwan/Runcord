e8eec236d10838d8ab133c40b60d90360599f64b
Kim-Junhwan <tazanboy1@naver.com>
2023-06-21 21:42:56 +0900

    [feat] 시간 관련 책임 가지는 TimeVO 구현

---
+++ Runcord/Sources/Common/Enum/TimeFormat.swift
@@ -0,0 +1,29 @@
+//
+//  StringFormat.swift
+//  Runcord
+//
+//  Created by JunHwan Kim on 2023/06/21.
+//
+
+enum TimeFormat {
+    case hourMinuteSecond
+}
+
+extension TimeFormat {
+    var timeFormatter: String {
+        switch self {
+        case .hourMinuteSecond:
+            return "%02d:%02d:%02d"
+        }
+    }
+    
+    func convertTimeToString(seconds: Int) -> String {
+        switch self {
+        case .hourMinuteSecond:
+            let hour = seconds / 3600
+            let minute = (seconds % 3600) / 60
+            let second = seconds % 60
+            return String(format: timeFormatter, hour, minute, second)
+        }
+    }
+}
